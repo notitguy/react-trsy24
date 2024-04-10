5241c74fe74be6ad1fa6c64636a932f73d660b7d
notitguy <notitguy@email.com>
2024-04-10 19:16:12 +0200

    react icons package

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  const names = ["mario", "yoshi", "luigi"];
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/JobListing.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const JobListing = ({ job }) => {
+  const [showFullDescription, setFullDescription] = useState(false);
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "..";
+  }
+  const setDescription = () => {
+    setFullDescription((toggle) => !toggle);
+  };
+
+  return (
+    <div className="bg-white rounded-xl shadow-md relative">
+      <div className="p-4 flex flex-col">
+        <div className="mb-6">
+          <div className="text-sm text-gray-600 my-2">{job.type}</div>
+          <h3 className="text-xl font-bold">{job.title}</h3>
+        </div>
+        <div>{description}</div>
+        <button
+          onClick={setDescription}
+          className="text-sm italic ml-auto text-gray-600 hover:text-indigo-600"
+        >
+          {showFullDescription ? "- less" : "+ more"}
+        </button>
+        <h3 className="mb-2">
+          <span className="text-indigo-500 font-bold">{job.salary}</span>/year
+        </h3>
+        <div className="border border-gray-100 mb-5"></div>
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="text-orange-700 mb-3">
+            <i className="fa-solid fa-location-dot text-lg"></i>
+            {job.location}
+          </div>
+          <a
+            href={`/jobs/${job.id}`}
+            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobListing;
